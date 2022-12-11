7dce1cb9e620bfc4dcdf7baf87735c60346ed396
FatemaAlbalushi <fatoomsaid188@gmail.com>
2022-12-10 23:58:55 -0800

    Add Tic-Tac-Toe Program

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,6 +15,63 @@ public class TicTacToe {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		char[][] arr= {{'1','2','3'},
+				{'4','5','6'},
+				{'7','8','9'}};
+		Scanner input = new Scanner(System.in);
+		char turn = 'X';
+		
+		System.out.println("Wellcome to Tic-Tac-Toe Program");
+
+		
+		
+		
+		for(int x =1;x<9;x++) {
+			
+			for(int i =0;i<arr.length;i++) 
+			{
+				for(int j =0;j<arr[i].length; j++) 
+				{
+					System.out.print(arr[i][j]+" ");
+					System.out.print("|"+" ");		
+				}
+				
+				System.out.println( );
+				System.out.println( "-----------");
+				
+				
+			}
+			
+			System.out.println("Player 1 Symbol: X");
+			System.out.println("Player 2 Symbol: O");
+			System.out.println("Make a move, Enter a position: ");
+			System.out.println("-------------------");
+			char position = input.next().charAt(0);
+			
+			
+				for(int i =0;i<arr.length;i++) 
+				{
+					for(int j =0;j<arr.length; j++) 
+					{
+						
+						 if(position==arr[i][j])  
+						{
+							if (turn=='X') 
+							{    
+			                    arr[i][j]='X';
+			                    turn ='O';
+			                }
+			                else
+			                {
+			                	 arr[i][j]='O';
+								 turn = 'X';
+			                }
+						}
+							
+					}		
+				}
+		}	
 
 	}
 
